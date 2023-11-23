a3ab67791639551988bc4ab1a34a8cdf93e3faf2
Ayomide Victor <midevictor90@gmail.com>
2023-11-23 10:02:42 +0100

    implemented the DBHome page that shows differnt biometrics of differnt orders that was made

--- Client/src/components/DBHome.jsx
+++ Client/src/components/DBHome.jsx
@@ -1,25 +1,90 @@
-import React, {useEffect} from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { getAllProduct } from '../api';
-import { setAllProducts } from '../context/actions/productAction';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getAllProduct } from "../api";
+import { setAllProducts } from "../context/actions/productAction";
+import { CChart } from "@coreui/react-chartjs";
 
 const DBHome = () => {
   const products = useSelector((state) => state.products);
   const dispatch = useDispatch();
 
-  useEffect (() => {
-    if(!products){
-      getAllProduct().then((data) => {
-        dispatch(setAllProducts(data))
-      })
-    }
+  const drinks = products?.filter((item) => item.product_category === "drinks");
+  const deserts = products?.filter(
+    (item) => item.product_category === "deserts"
+  );
+  const fruits = products?.filter((item) => item.product_category === "fruits");
+  const rice = products?.filter((item) => item.product_category === "rice");
+  const curry = products?.filter((item) => item.product_category === "curry");
+  const chinese = products?.filter(
+    (item) => item.product_category === "chinese"
+  );
+  const bread = products?.filter((item) => item.product_category === "bread");
 
+  useEffect(() => {
+    if (!products) {
+      getAllProduct().then((data) => {
+        dispatch(setAllProducts(data));
+      });
+    }
   }, []);
   return (
-    <div>
-      hhhhhl
+    <div className="flex items-center justify-center flex-col pt-6 w-full h-full">
+      <div className="grid w-full grid-cols-1 md:grid-cols-2 gap-4 h-full">
+        <div className="flex items-center justify-center">
+          <div className="w-340 md:w-508">
+            <CChart
+              type="bar"
+              data={{
+                labels: [
+                  "Drinks",
+                  "Deserts",
+                  "Fruits",
+                  "Rice",
+                  "Curry",
+                  "Bread",
+                  "Chinese",
+                ],
+                datasets: [
+                  {
+                    label: "Category Wise Count",
+                    backgroundColor: "#f87979",
+                    data: [drinks?.length, deserts?.length, fruits?.length, rice?.length, curry?.length, bread?.length, chinese?.length],
+                  },
+                ],
+              }}
+              labels="months"
+            />
+          </div>
+        </div>
+        <div className="w-full h-full flex items-center justify-center">
+          <div className="w-275 md:w-460">
+            <CChart
+              type="polarArea"
+              data={{
+                labels: ["Orders",
+                "Delivered",
+                "Cancelled",
+                "Paid",
+                "Not Paid",],
+                datasets: [
+                  {
+                    data: [11, 16, 7, 3, 14],
+                    backgroundColor: [
+                      "#51FF00",
+                      "#00B6FF",
+                      "#008BFF",
+                      "#FFD100",
+                      "#FF00FB",
+                    ],
+                  },
+                ],
+              }}
+            />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DBHome
+export default DBHome;
